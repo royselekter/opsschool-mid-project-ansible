c3fbdfb1c11d4c74f1594c935d14454bca2e1093
yishai <yishai5555@gmail.com>
2019-06-03 22:14:00 +0300

    add-ansible-install

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
  
-ansible-playbook -i inventory.yml ansible-playbooks/install-docker.yml 
+ansible-playbook -i inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-docker.yml 
 
-ansible-playbook -i inventory.yml ansible-playbooks/k8s-common.yml 
-ansible-playbook -i inventory.yml ansible-playbooks/k8s-master.yml 
-ansible-playbook -i inventory.yml ansible-playbooks/k8s-minion.yml 
+ansible-playbook -i inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/k8s-common.yml 
+ansible-playbook -i inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/k8s-master.yml 
+ansible-playbook -i inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/k8s-minion.yml 
