9488686af8e8b7923a840a62269bdd679bae2113
yishai <yishai5555@gmail.com>
2019-06-07 16:27:53 +0300

    add-test-of-ansible

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
  
+ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-local-test.yml 
+
 ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-docker.yml 
 
 ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/k8s-common.yml 
