08d58d00ba4f732c92d8f23502774775a86f5f07
yishai <yishai5555@gmail.com>
2019-06-07 17:47:38 +0300

    add-test-of-ansible-with-retry

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,6 +1,26 @@
 #! /bin/bash
  
-ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-local-test.yml 
+
+
+I=0
+
+while true
+do
+  echo $I
+    
+    ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-local-test.yml   
+  
+  if [[ $I -gt 9 ]];then
+    break
+  fi
+  I=$((I+1))
+  sleep 2
+done
+
+echo "finished"
+
+
+# ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-local-test.yml 
 
 ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-docker.yml 
 
