802ac58b9d28b4b3337374ac306833847bace1ef
yishai <yishai5555@gmail.com>
2019-06-10 08:54:20 +0300

    install-ansible-before-run-playbook

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,23 +1,28 @@
 #! /bin/bash
  
+apt-add-repository ppa:ansible/ansible -y
+apt-get update -y
+apt-get install ansible -y
 
 
-I=0
 
-while true
-do
-  echo $I
+
+# I=0
+
+# while true
+# do
+#   echo $I
     
-    ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-local-test.yml   
+#     ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-local-test.yml   
   
-  if [[ $I -gt 9 ]];then
-    break
-  fi
-  I=$((I+1))
-  sleep 2
-done
+#   if [[ $I -gt 9 ]];then
+#     break
+#   fi
+#   I=$((I+1))
+#   sleep 2
+# done
 
-echo "finished"
+# echo "finished"
 
 
 # ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-local-test.yml 
