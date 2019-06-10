8c2f62f4c5a4b0223db26d6c4ace6a1515c56cc4
yishai <yishai5555@gmail.com>
2019-06-10 22:09:55 +0300

    add-chown

--- run-ansible-playbooks.sh
+++ run-ansible-playbooks.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 
-
+ sudo chown -R ubuntu /home/ubuntu/.ansible 
 
 ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-docker.yml 
 
