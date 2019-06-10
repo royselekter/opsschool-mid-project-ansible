94c2076e4d13db133d12d3968d91f759eafb26e6
yishai <yishai5555@gmail.com>
2019-06-10 21:20:52 +0300

    add-chown

--- install-ansible.sh
+++ install-ansible.sh
@@ -35,6 +35,8 @@ apt-get install ansible -y
 
 # echo "finished"
 
+sleep 60
+
 
 # ansible-playbook -i /home/ubuntu/opsschool-mid-project-ansible/inventory.yml /home/ubuntu/opsschool-mid-project-ansible/ansible-playbooks/install-local-test.yml 
 
