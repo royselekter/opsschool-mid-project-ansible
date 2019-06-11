8103466e1451763c3dd84c22d813e66c13374530
yishai <yishai5555@gmail.com>
2019-06-11 10:38:01 +0300

    set-private-key

--- set-ansible-private-key.sh
+++ set-ansible-private-key.sh
@@ -2,4 +2,4 @@
 
 # ssh -o stricthostkeychecking=no ubuntu@192.168.15.20 'sed -i "s/#private_key_file/private_key_file=/home/ubuntu/.ssh/id_rsa#/g" /etc/ansible/ansible.cfg' 
 
-sudo sed -i "s/#private_key_file/private_key_file=\/home\/ubuntu\/.ssh\/id_rsa #/g" /etc/ansible/ansible.cfg
+sudo sed -i "s/#private_key_file*/private_key_file=\/home\/ubuntu\/.ssh\/id_rsa/g" /etc/ansible/ansible.cfg
